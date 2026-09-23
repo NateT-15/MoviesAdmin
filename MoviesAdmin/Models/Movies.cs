1be23b96cce8331206762b5ff5d4d351f38f0381
Nate Trask <w0523024@nscc.ca>
2026-09-23 12:44:56 -0300

    Created a basic movies model. This will be updated as I continue to develop the project.

---
+++ MoviesAdmin/Models/Movies.cs
@@ -0,0 +1,11 @@
+﻿namespace MoviesAdmin.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Genre { get; set; } = string string.Empty;
+        public DateTime ReleaseDate { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
