40814980c38534661cac97254ec797114014686c
Nate Trask <w0523024@nscc.ca>
2026-09-24 11:44:06 -0300

    Fixed an error in the movies model.

--- MoviesAdmin/Models/Movies.cs
+++ MoviesAdmin/Models/Movies.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
-        public string Genre { get; set; } = string string.Empty;
+        public string Genre { get; set; } = string.Empty;
         public DateTime ReleaseDate { get; set; }
         public string Description { get; set; } = string.Empty;
     }
